ecdc04ffa3bddc66c1f5eedfeec47aa0c14680c6
July Pilowsky <japilo@users.noreply.github.com>
2024-08-02 12:50:09 -0400

    foreach explicitly exports metaRange to parallel threads

--- R/Class_Parallel.r
+++ R/Class_Parallel.r
@@ -256,7 +256,7 @@ metaRangeParallel <- R6::R6Class("metaRangeParallel",
       }
 
       simulation_log <- foreach(i = 1:nrow(self$sample_data),
-                          .packages = c("raster"),
+                          .packages = c("raster", "metaRange"),
                           .export = c("self"),
                           .errorhandling = c("pass")) %dopar% {
 
